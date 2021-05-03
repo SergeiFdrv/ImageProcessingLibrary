12caafea09b996157fdfe759e7fc393692039f2c
sergfedoro@mail.ru <sergfedoro@mail.ru>
2021-05-03 12:17:56 +0300

    Добавьте файлы проекта.

---
+++ ImageToText.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ImageToText.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            ImageToTextLibrary.Converter converter = new ImageToTextLibrary.Converter();
+            do
+            {
+                Console.WriteLine("Введите путь к файлу:");
+                path = Console.ReadLine();
+                if (System.IO.File.Exists(path))
+                {
+                    Console.WriteLine(
+                        "--- Результат: ---\n" +
+                        converter.DetectText(path) +
+                        "\n------------------");
+                }
+                else
+                {
+                    Console.WriteLine("Файл не найден");
+                }
+                Console.WriteLine("Нажмите Escape, чтобы выйти");
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ ImageToTextLibrary/Converter.cs
@@ -0,0 +1,41 @@
+﻿using Emgu.CV;
+using Emgu.CV.OCR;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+
+namespace ImageToTextLibrary
+{
+    public class Converter
+    {
+        public string DetectText(string imgPath)
+        {
+            string text;
+            using (var img = new Image<Bgr, byte>(imgPath))
+            {
+                string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\tessdata\";
+                // Языковые пакеты: https://github.com/tesseract-ocr/tessdata
+                using (var ocrProvider = new Tesseract(path, "rus", OcrEngineMode.TesseractLstmCombined))
+                {
+                    ocrProvider.SetImage(img);
+                    ocrProvider.Recognize();
+                    text = ocrProvider.GetUTF8Text().TrimEnd();
+                }
+            }
+            return text;
+        }
+
+        public bool DetectFace(string path)
+        {
+            /*
+            var HaarCascadeXML = new HaarCascade("haarcascade_frontalface_alt.xml");
+            var faces = HaarCascadeXML.Detect(face, 1.1, 10, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                                              new Size(20, 20), new Size(BaseImage.Width, BaseImage.Height));
+            */
+            return true;
+        }
+    }
+}
